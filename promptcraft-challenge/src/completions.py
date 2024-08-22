cce53d6648113cec39c67ff12ce3b5236755f61b
Aman <training@datacouch.io>
2024-08-22 22:43:28 +0530

    Delete completions.py

--- promptcraft-challenge/src/completions.py
+++
@@ -1,9 +0,0 @@
-import openai
-
-def get_openai_completion(messages):
-    response = openai.ChatCompletion.create(
-        model="gpt-4",
-        messages=messages,
-        temperature=0
-    )
-    return response['choices'][0]['message']['content']
